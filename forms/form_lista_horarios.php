9829807b85a1a7c534b0e89dab3773e88623c24f
Jose Ponce <albertojoseponce@gmail.com>
2019-02-04 10:56:11 -0300

    se agrega 2019

--- forms/form_lista_horarios.php
+++ forms/form_lista_horarios.php
@@ -12,7 +12,8 @@
                     <select id="anio" onchange="buscar_horarios()">
                         <option value='2016'>2016</option>
                         <option value='2017'>2017</option>
-                        <option value='2018' selected="selected">2018</option>
+                        <option value='2018'>2018</option>
+                        <option value='2019' selected="selected">2019</option>
                     </select>
                 </div>
             </div>
